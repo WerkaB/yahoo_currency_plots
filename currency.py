948aabdd40a446601dcfe1ef1c1271f5d7c59546
Weronika <weronika.borucka@gmail.com>
2023-03-04 19:10:00 +0100

    feat: :sparkles: calculate and draw SMA

--- currency.py
+++ currency.py
@@ -1,10 +1,10 @@
 import pandas_datareader.yahoo.fx
 import datetime
-import pandas
 import matplotlib.pyplot as plt
 
 end_date = datetime.date.today()
-start_date = end_date - datetime.timedelta(days=5)
+# w notatkach mam zapisane "za ostatni miesiąc", zakładam że jest to ostatnie 30 dni.
+start_date = end_date - datetime.timedelta(days=20)
 
 # from: https://pandas-datareader.readthedocs.io/en/latest/readers/yahoo.html#pandas_datareader.yahoo.fx.YahooFXReader
 eurusd_reader = pandas_datareader.yahoo.fx.YahooFXReader(symbols="EURUSD", start=start_date, end=end_date, interval='d')
@@ -13,6 +13,13 @@ print(eurusd)
 print(eurusd.loc[datetime.date.today() - datetime.timedelta(days=2)])
 print(eurusd.info())
 
+
+# Calculating simple moving average
+eurusd = eurusd["Close"].to_frame()
+eurusd["SMA5"] = eurusd['Close'].rolling(5).mean()
+print(eurusd)
+
+# Draw the plot!
 eurusd.plot()
 plt.show()
 
