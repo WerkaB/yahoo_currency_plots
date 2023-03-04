b6d71931084c4b0fc314ab38fceb3802405795dc
Weronika <weronika.borucka@gmail.com>
2023-03-04 18:55:10 +0100

    feat: :sparkles: Draw a plot from yahoo data

--- currency.py
+++ currency.py
@@ -1,12 +1,19 @@
 import pandas_datareader.yahoo.fx
 import datetime
 import pandas
+import matplotlib.pyplot as plt
 
 end_date = datetime.date.today()
-start_date = end_date - datetime.timedelta(days=30)
+start_date = end_date - datetime.timedelta(days=5)
 
-eurusd_reader = pandas_datareader.yahoo.fx.YahooFXReader(symbols="EURUSD", start=start_date, end=end_date, interval='d',chunksize=7)
+# from: https://pandas-datareader.readthedocs.io/en/latest/readers/yahoo.html#pandas_datareader.yahoo.fx.YahooFXReader
+eurusd_reader = pandas_datareader.yahoo.fx.YahooFXReader(symbols="EURUSD", start=start_date, end=end_date, interval='d')
 eurusd = eurusd_reader.read()
 print(eurusd)
+print(eurusd.loc[datetime.date.today() - datetime.timedelta(days=2)])
+print(eurusd.info())
+
+eurusd.plot()
+plt.show()
 
 
