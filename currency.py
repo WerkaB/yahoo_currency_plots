5f7b198e05d4a1942b01d4c4f1556f927750575a
Weronika <weronika.borucka@gmail.com>
2023-03-05 17:49:26 +0100

    refactor: :recycle: add some comments

--- currency.py
+++ currency.py
@@ -6,7 +6,7 @@ import dateutil.relativedelta
 import pandas
 
 currency_exrate = "EURUSD"
-moving_average_type = "EMA"
+moving_average_type = "SMA"
 
 # Dane dla średniej kroczącej 15 i 40 dni
 step_length = [15, 40]
@@ -14,15 +14,17 @@ step_length = [15, 40]
 # Zakładam że "za ostatni miesiąc" oznacza poprzedni miesiąc od 1-go do ostatniego dnia miesiąca.
 current_date = datetime.date.today()
 
-previous_month_1_day = (current_date - dateutil.relativedelta.relativedelta(months=1)).replace(day=1)  # 1st day for plot
-last_day_of_previous_month = calendar.monthrange(previous_month_1_day.year, previous_month_1_day.month)[1]
-previous_month_last_day = previous_month_1_day.replace(day=last_day_of_previous_month)  # last day for plot
+# First day of the month - 1st day for plot
+first_month_day = (current_date - dateutil.relativedelta.relativedelta(months=1)).replace(day=1)
+last_day_of_previous_month = calendar.monthrange(first_month_day.year, first_month_day.month)[1]
+# Last day of the month - last day of plot
+last_month_day = first_month_day.replace(day=last_day_of_previous_month)
 
 # Aby móc narysować wykres średnich kroczących potrzebujemy danych sprzed okresu który nas interesuje
-max_prev_days = previous_month_1_day - datetime.timedelta(days=max(step_length))
+max_prev_days = first_month_day - datetime.timedelta(days=max(step_length))
 
 start_date = max_prev_days
-end_date = previous_month_last_day
+end_date = last_month_day
 
 # from: https://pandas-datareader.readthedocs.io/en/latest/readers/yahoo.html#pandas_datareader.yahoo.fx.YahooFXReader
 eur_exrate_reader = pandas_datareader.yahoo.fx.YahooFXReader(symbols=currency_exrate,
@@ -33,23 +35,28 @@ print(eur_exrate.info())
 # We will work on single column for simplifying
 eur_exrate = eur_exrate["Close"].to_frame()
 
+# We need to fill lacking date values, on weekends are no currency exchange rate information - expand index in dataframe
+# Create new date range
 new_date_idx = pandas.date_range(start_date, end_date)
+# Replace old index in dataframe with filled one
 reindexed_eur_exrate = eur_exrate.reindex(new_date_idx, fill_value=None)
-filled_eur_exrate = reindexed_eur_exrate.fillna(method='pad')
+# Fill None values from expanding index, with previous values (method pad)
+eur_exrate = reindexed_eur_exrate.fillna(method='pad')
 
 # Calculating simple or exponential moving average for Close
 for step in step_length:
     if moving_average_type == "SMA":
-        filled_eur_exrate[f"{moving_average_type}{step}"] = filled_eur_exrate['Close'].rolling(step).mean()
+        eur_exrate[f"{moving_average_type}{step}"] = eur_exrate['Close'].rolling(step).mean()
     elif moving_average_type == "EMA":
-        filled_eur_exrate[f"{moving_average_type}{step}"] = filled_eur_exrate['Close'].ewm(span=step).mean()
+        eur_exrate[f"{moving_average_type}{step}"] = eur_exrate['Close'].ewm(span=step).mean()
     else:
         print("You shall not pass! \U0001F9D9")
         break
 
+# Change column name
+eur_exrate.rename(columns={'Close': currency_exrate}, inplace=True)
+# Limit plot range for full month
+eur_exrate_previous_month = eur_exrate.loc[first_month_day:last_month_day]
 # Draw the plot!
-eur_exrate_previous_month = filled_eur_exrate.loc[previous_month_1_day:previous_month_last_day]
-eur_exrate_previous_month.plot()
+eur_exrate_previous_month.plot(marker='.')
 plt.show()
-
-
